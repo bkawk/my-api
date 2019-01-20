9dcd38ceb943557b89ad48904801d4f4cbf881ee
bkawk <email.will.in.china@gmail.com>
2019-01-20 15:11:20 +0800

    change

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(helmet());
 
-app.get('/', (req, res) => res.send('API Home'));
+app.get('/', (req, res) => res.send('API Home2'));
 app.listen(3000, () => console.log('API @ http://localhost:3000')); // eslint-disable-line
 
 module.exports = app;
