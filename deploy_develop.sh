22897ae14c214f2524eb0fd341f759ed69fd5e6a
bkawk <email.will.in.china@gmail.com>
2019-01-20 17:25:12 +0800

    test hello

--- deploy_develop.sh
+++ deploy_develop.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Deploy to develop
+echo "Hello World!"
 $(aws ecr get-login --no-include-email --region us-east-1)
 docker build -t develop-my-api .
 docker tag develop-my-api:latest 989002346127.dkr.ecr.us-east-1.amazonaws.com/develop-my-api:latest
--- deploy_production.sh
+++ deploy_production.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Deploy to production
+echo "Hello World!"
 $(aws ecr get-login --no-include-email --region us-east-1)
 docker build -t production-my-api .
 docker tag production-my-api:latest 989002346127.dkr.ecr.us-east-1.amazonaws.com/production-my-api:latest
